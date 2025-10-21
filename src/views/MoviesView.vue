78490ba4c588980b144e549d0371d073c4365481
analuizakohler <analuizakohler24@gmail.com>
2025-10-21 12:00:49 -0300

    eu amo o professor

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,40 +1,50 @@
 <script setup>
   import { ref, onMounted } from 'vue';
   import api from '@/plugins/axios';
+  import Loading from 'vue-loading-overlay';
+  import { useGenreStore } from '@/stores/genre';
+
+  const genreStore = useGenreStore();
+
+  const isLoading = ref(false);
 
   const movies = ref([]);
 
-  const listMovies = async (genreId) => {
-      const response = await api.get('discover/movie', {
-          params: {
-              with_genres: genreId,
-              language: 'pt-BR'
-          }
-      });
-      movies.value = response.data.results
-  };
-
-  const genres = ref([]);
 
   onMounted(async () => {
-    const response = await api.get('genre/movie/list?language=pt-BR');
-    genres.value = response.data.genres;
-  });
-  import Loading from 'vue-loading-overlay';
+  isLoading.value = true;
+  await genreStore.getAllGenres('movie');
+  isLoading.value = false;
+});
 
-  const isLoading = ref(false);
+  const listMovies = async (genreId) => {
+  isLoading.value = true;
+  const response = await api.get('discover/movie', {
+    params: {
+      with_genres: genreId,
+      language: 'pt-BR'
+    }
+  });
+  movies.value = response.data.results
+  isLoading.value = false;
+};
+
+
+
+  const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
+  
 </script>
 <template>
   <h1>Filmes</h1>
   <ul class="genre-list">
     <li
-    v-for="genre in genres"
-    :key="genre.id"
-    @click="listMovies(genre.id)"
-    class="genre-item"
-  >
-    {{ genre.name }}
-  </li>
+  v-for="genre in genreStore.genres"
+  :key="genre.id"
+  @click="listMovies(genre.id)"
+  class="genre-item"
+>
+  {{ genre.name }}
+</li>
   <div class="movie-list">
   <div v-for="movie in movies" :key="movie.id" class="movie-card">
     <img
@@ -43,8 +53,16 @@
     />
     <div class="movie-details">
       <p class="movie-title">{{ movie.title }}</p>
-      <p class="movie-release-date">{{ movie.release_date }}</p>
-      <p class="movie-genres">{{ movie.genre_ids }}</p>
+      <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
+      <p class="movie-genres">
+  <span
+  v-for="genre_id in movie.genre_ids"
+  :key="genre_id"
+  @click="listMovies(genre_id)"
+>
+  {{ genreStore.getGenreName(genre_id) }}
+</span>
+</p>
     </div>
   </div>
 </div>
@@ -103,5 +121,28 @@
   font-weight: bold;
   line-height: 1.3rem;
   height: 3.2rem;
+}
+.movie-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+}
+
+.movie-genres span {
+  background-color: #748708;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+
+.movie-genres span:hover {
+  cursor: pointer;
+  background-color: #455a08;
+  box-shadow: 0 0 0.5rem #748708;
 }
 </style>
--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,47 +1,121 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import api from '@/plugins/axios';
+import { ref, onMounted } from 'vue'
+import api from '@/plugins/axios'
+const tvshow = ref([])
 
-  const genres = ref([]);
+const listTV = async (with_genres) => {
+  const response = await api.get('discover/tv', {
+    params: {
+      with_genres: with_genres,
+      language: 'pt-BR',
+    },
+  })
+  tvshow.value = response.data.results
+}
 
-  onMounted(async () => {
-    const response = await api.get('genre/tv/list?language=pt-BR');
-    genres.value = response.data.genres;
-  });
+const genres = ref([])
+
+onMounted(async () => {
+  const response = await api.get('genre/tv/list?language=pt-BR')
+  genres.value = response.data.genres
+})
 </script>
 
 <template>
   <h1>Programas de TV</h1>
   <ul class="genre-list">
-    <li v-for="genre in genres" :key="genre.id" class="genre-item">
+    <li v-for="genre in genres" :key="genre.id" @click="listTV(genre.id)" class="genre-item">
       {{ genre.name }}
     </li>
   </ul>
+  <div class="tv-list">
+    <div v-for="tvshows in tvshow" :key="tvshows.id" class="tvshows-card">
+      <img :src="`https://image.tmdb.org/t/p/w500${tvshows.poster_path}`" :alt="tvshows.title" />
+      <div class="tvshows-details">
+        <p class="tvshows-title">{{ tvshows.name }}</p>
+        <p class="tvshows-release-date">{{ tvshows.first_air_date }}</p>
+        <p class="tvshows-genres">{{ tvshows.with_genres }}</p>
+        <p class="tvshows-original-name">{{ tvshows.original_name }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-  .genre-list {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 2rem;
-    list-style: none;
-    padding: 0;
-  }
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  margin-bottom: 2rem;
+}
 
-  .genre-item {
-    background-color: #5d6424;
-    border-radius: 1rem;
-    padding: 0.5rem 1rem;
-    align-self: center;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-  }
+.genre-item {
+  background-color: #7d8a2e;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  color: #fff;
+}
 
-  .genre-item:hover {
-    cursor: pointer;
-    background-color: #7d8a2e;
-    box-shadow: 0 0 0.5rem #5d6424;
-  }
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #7d8a2e;
+  box-shadow: 0 0 0.5rem #5d6424;
+}
+
+.tv-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.tvshows-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tvshows-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tvshows-details {
+  padding: 0 0.5rem;
+}
+
+.tvshows-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}
+.tvshows-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+}
+
+.tvshows-genres span {
+  background-color: #748708;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+
+.tvshows-genres span:hover {
+  cursor: pointer;
+  background-color: #455a08;
+  box-shadow: 0 0 0.5rem #748708;
+}
 </style>
