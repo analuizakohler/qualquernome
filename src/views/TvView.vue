ac038e176ef1d0809a0c0c0f683e9122e4ad7f99
analuizakohler <analuizakohler24@gmail.com>
2025-10-23 10:56:07 -0300

    arrumando coisinha

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,41 +1,55 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import api from '@/plugins/axios'
-const tvshow = ref([])
+const tvshows = ref([])
+const genres = ref([])
 
-const listTV = async (with_genres) => {
+const listTV = async (genreId) => {
   const response = await api.get('discover/tv', {
     params: {
-      with_genres: with_genres,
+      with_genres: genreId,
       language: 'pt-BR',
     },
   })
-  tvshow.value = response.data.results
+  tvshows.value = response.data.results
 }
 
-const genres = ref([])
+
 
 onMounted(async () => {
   const response = await api.get('genre/tv/list?language=pt-BR')
   genres.value = response.data.genres
 })
+
+const getGenreName = (id) =>
+ genres.value.find((genre) => genre.id === id)?.name || '';
+ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
 </script>
 
 <template>
   <h1>Programas de TV</h1>
   <ul class="genre-list">
     <li v-for="genre in genres" :key="genre.id" @click="listTV(genre.id)" class="genre-item">
-      {{ genre.name }}
+      {{ genre.name }} 
     </li>
   </ul>
   <div class="tv-list">
-    <div v-for="tvshows in tvshow" :key="tvshows.id" class="tvshows-card">
-      <img :src="`https://image.tmdb.org/t/p/w500${tvshows.poster_path}`" :alt="tvshows.title" />
-      <div class="tvshows-details">
-        <p class="tvshows-title">{{ tvshows.name }}</p>
-        <p class="tvshows-release-date">{{ tvshows.first_air_date }}</p>
-        <p class="tvshows-genres">{{ tvshows.with_genres }}</p>
-        <p class="tvshows-original-name">{{ tvshows.original_name }}</p>
+    <div v-for="tv in tvshows" :key="tv.id" class="tv-card">
+      <img :src="`https://image.tmdb.org/t/p/w500${tv.poster_path}`" :alt="tv.title" />
+      <div class="tv-details">
+        <p class="tv-title">{{ tv.name }}</p>
+        <p class="tv-original-name">{{ tv.original_name }}</p>
+        <p class="tv-release-date">{{formatDate(tv.first_air_date)}}</p>
+        <p class="tv-genres">
+        <span
+        v-for="genre_id in tv.genre_ids"
+        :key="genre_id"
+        @click="listTV(genre_id)"
+        >
+        {{ getGenreName(genre_id) }}
+        </span>
+        </p>
+        
       </div>
     </div>
   </div>
@@ -70,7 +84,7 @@ onMounted(async () => {
   gap: 1rem;
 }
 
-.tvshows-card {
+.tv-card {
   width: 15rem;
   height: 30rem;
   border-radius: 0.5rem;
@@ -78,24 +92,24 @@ onMounted(async () => {
   box-shadow: 0 0 0.5rem #000;
 }
 
-.tvshows-card img {
+.tv-card img {
   width: 100%;
   height: 20rem;
   border-radius: 0.5rem;
   box-shadow: 0 0 0.5rem #000;
 }
 
-.tvshows-details {
+.tv-details {
   padding: 0 0.5rem;
 }
 
-.tvshows-title {
+.tv-title {
   font-size: 1.1rem;
   font-weight: bold;
   line-height: 1.3rem;
   height: 3.2rem;
 }
-.tvshows-genres {
+.tv-genres {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -104,7 +118,7 @@ onMounted(async () => {
   gap: 0.2rem;
 }
 
-.tvshows-genres span {
+.tv-genres span {
   background-color: #748708;
   border-radius: 0.5rem;
   padding: 0.2rem 0.5rem;
@@ -113,7 +127,7 @@ onMounted(async () => {
   font-weight: bold;
 }
 
-.tvshows-genres span:hover {
+.tv-genres span:hover {
   cursor: pointer;
   background-color: #455a08;
   box-shadow: 0 0 0.5rem #748708;
